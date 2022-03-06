d6c8768991629aaf8c06de8c17817055d9516094
bmtechua <bmtech@i.ua>
2022-03-06 16:50:28 +0200

    Initial Commit

---
+++ UIKit_lesson_25_UITableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit_lesson_25_UITableView
+//
+//  Created by bmtech on 06.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
